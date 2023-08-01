530d18fb18b6c4957acad49bd30782ad56b31b31
Amruta-Nadgonde <amruta.nadgonde@gmail.com>
2023-08-01 19:18:11 +0530

    added CalculatorTest

---
+++ src/test/java/tests/unit/CalcultorNewTest.java
@@ -0,0 +1,14 @@
+package tests.unit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalcultorNewTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
